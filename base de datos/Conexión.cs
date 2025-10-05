da1b09d6e8852064035d972606b79601c65d6e22
Ryan Durán Rey <100086388@p.uapa.edu.do>
2025-10-04 22:52:24 -0400

    Creación de clase Conexión.cs

---
+++ base de datos/Conexión.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Npgsql;
+
+namespace Proyecto.base_de_datos
+{
+    public class Conexion
+    {
+        private string conexionString;
+
+        public Conexion()
+        {
+            conexionString = "Host=localhost;Port=5432;Username=postgres;Password=22022006;Database=";
+        }
+
+        public NpgsqlConnection GetConnection() 
+        {
+            try
+            {
+                NpgsqlConnection conn = new NpgsqlConnection(conexionString);
+                return conn;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("No se pudo conectar con la base de datos:" + ex.Message);
+            }
+        }
+    }
+}
--- obj/Release/net8.0-windows/Proyecto.AssemblyInfo.cs
+++ obj/Release/net8.0-windows/Proyecto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd528ce7afad1830e2758b985e4a2509890d89ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a68265e019ce8d9312f4520fc666ce07ab338c87")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
